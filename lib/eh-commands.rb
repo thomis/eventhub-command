cc60d69dabdc25af96eb7d64616aaa657dcf7c49
Pascal Betz <pascal.betz@simplificator.com>
2014-09-25 15:07:18 +0200

    don’t require release command as it has been removed

--- lib/eh-commands.rb
+++ lib/eh-commands.rb
@@ -2,6 +2,5 @@
 # All commands are required here
 require 'eh/commands/copy_config'
 require 'eh/commands/generate_processor'
-require 'eh/commands/release'
 require 'eh/commands/package_rails'
 require 'eh/commands/package'
