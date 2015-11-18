526dcae83c9069f8329ba5c5d2c5431b88bc82d7
Pascal Betz <pascal.betz@simplificator.com>
2015-11-18 16:19:03 +0100

    grouped commands.

--- lib/deployer/console_deployer.rb
+++ lib/deployer/console_deployer.rb
@@ -1,45 +1,24 @@
 class Deployer::ConsoleDeployer < Deployer::BaseDeployer
 
+  def initialize(options)
+    @options = options
+  end
+
   def deploy!
-    case deploy_via
-    when 'scp' then deploy_via_scp
-    when 'svn' then deploy_via_svn
-    else raise "Unknown value for deploy_via: #{deploy_via}"
+    Bundler.with_clean_env do
+      Dir.chdir(console_dir) do
+        system "bundle install && cap #{stage} deploy"
+      end
     end
   end
 
   private
 
-  def working_dir
-    options[:working_dir]
+  def console_dir
+    Eh::Settings.current.console_source_dir
   end
 
-  def deploy_via_scp
-    cmd = "cd #{working_dir} && bundle install && bundle exec cap #{stage.name} deploy"
-    execute_in_clean_env(cmd)
+  def stage
+    @options.fetch(:stage, 'development')
   end
-
-  def deploy_via_svn
-    console_release_zip = '~/apps/event_hub/shared/cached_copy_svn/branches/master/releases/rails/console.zip'
-    cmds = []
-    cmds << "rm -rf /tmp/console_deployment && mkdir -p /tmp/console_deployment"
-    cmds << "cd /tmp/console_deployment && unzip #{console_release_zip} -d ."
-    cmds << "cd /tmp/console_deployment/console && bundle install && bundle exec cap #{stage.name} deploy"
-    # run the command only on one host of this stage.
-    Deployer::Executor.new(stage.single_host_stage, verbose: verbose?) do |executor|
-      update_scm(executor)
-      cmds.each do |cmd|
-        executor.execute(cmd)
-      end
-    end
-  end
-
-  def execute_in_clean_env(cmd)
-    puts "Will run #{cmd} on your local machine"
-    Bundler.with_clean_env do
-      system cmd
-    end
-  end
-
-
 end
--- lib/eh/commands/deploy.rb
+++ lib/eh/commands/deploy.rb
@@ -1,12 +1,13 @@
-desc 'deployment'
-
+desc 'deployment commands'
 command :deploy do |c|
   c.flag([:stage], desc: 'stage', type: String, long_desc: 'Stage where processor is deployed to', default_value: Eh::Settings.current.default_stage)
   c.flag([:branch], desc: 'branch', type: String, long_desc: 'What branch to deploy. Only when deploy_via=scm', default_value: 'master')
   c.flag([:tag], desc: 'tag', type: String, long_desc: 'What tag to deploy. Only when deploy_via=scm', default_value: nil)
+  c.switch([:v, :verbose], :desc => 'Show additional output.')
+  c.flag([:deploy_via], desc: 'where to deploy from', type: String, long_desc: 'deploy via scm or scp. If you use scp then the working_dir is packaged and copied tot the servers', default_value: 'svn')
 
   c.command :all do |c|
-
+    c.desc 'deploy all'
     c.action do |global_options, options, arguments|
       forward_arguments = arguments.join(' ')
       deploy_config(options, forward_arguments)
@@ -16,19 +17,71 @@ command :deploy do |c|
     end
   end
 
+  c.command :config do |c|
+    c.desc 'distribute the configs to the nodes'
+    c.action do |global_options, options, args|
+      Deployer::ConfigDeployer.new(options).deploy!
+    end
+  end
+
+  desc 'deploy the rails console app'
+  c.command :console do |c|
+    c.flag([:working_dir], desc: 'directory to execute commands in', type: String, default_value: '.')
+
+    c.action do |global_options, options, args|
+      Deployer::ConsoleDeployer.new(options).deploy!
+    end
+  end
+
+
+  c.command :mule do |c|
+    c.desc 'deploy a single channel adapter'
+    c.arg_name '[channel_adapter[,other_channel_adapter,pattern*]]'
+
+    c.action do |global_options, options, args|
+      begin
+        if args[0]
+          adapter_names = args[0].split(',').map(&:strip)
+        else
+          adapter_names = nil
+        end
+        Deployer::MuleDeployer.new(adapter_names, options).deploy!
+      rescue => e
+        puts e.message
+        puts e.backtrace.join("\n")
+      end
+    end
+  end
+
+  c.command :ruby do |c|
+    c.desc 'deploy a single ruby processor'
+    c.arg_name '[processor_name,[other_processor_name,pattern*]]'
+
+    c.action do |global_options, options, args|
+      begin
+        if args[0]
+          processor_names = args[0].split(',').map(&:strip)
+        else
+          processor_names = nil
+        end
+        Deployer::RubyDeployer.new(processor_names, options).deploy!
+      rescue => e
+        puts e.message
+        puts e.backtrace.join("\n")
+      end
+    end
+  end
+
+
 
   private
 
   def deploy_ruby(options, forward_arguments)
-    system "#{extend_command(:deploy_ruby)} #{copy_options(options, :stage, :branch, :tag, :verbose)} #{forward_arguments}"
+    system "#{extend_command('deploy ruby')} #{copy_options(options, :stage, :branch, :tag, :verbose)} #{forward_arguments}"
   end
 
   def deploy_console(options, forward_arguments)
-    Bundler.with_clean_env do
-      Dir.chdir(console_dir) do
-        system "bundle install && cap #{options[:stage]} deploy"
-      end
-    end
+    system "#{extend_command('deploy console')} #{copy_options(options, :stage)} #{forward_arguments}"
   end
 
   def deploy_mule(options, forward_arguments)
@@ -36,7 +89,7 @@ command :deploy do |c|
   end
 
   def deploy_config(options, forward_arguments)
-    system "#{extend_command(:deploy_config)} #{copy_options(options, :stage, :branch, :tag, :verbose)} #{forward_arguments}"
+    system "#{extend_command('deploy config')} #{copy_options(options, :stage, :branch, :tag, :verbose)} #{forward_arguments}"
   end
 
   def copy_option(options, option)
@@ -55,7 +108,4 @@ command :deploy do |c|
     "bundle exec eh #{command}"
   end
 
-  def console_dir
-    Eh::Settings.current.console_source_dir
-  end
 end
